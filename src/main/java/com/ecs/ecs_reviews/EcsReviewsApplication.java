dfe537ffe03e6e93f98566186f5605b7c869dad6
= <=>
2024-09-26 08:53:44 -0400

    Initial commit from local

---
+++ src/main/java/com/ecs/ecs_reviews/EcsReviewsApplication.java
@@ -0,0 +1,13 @@
+package com.ecs.ecs_reviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsReviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsReviewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecs/ecs_reviews/EcsReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecs.ecs_reviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsReviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
